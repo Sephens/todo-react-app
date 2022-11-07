9b3eb15d3adc05fbec574e8a3b3fb91adb450ecc
Sephens <adesephens@gmail.com>
2022-11-07 12:13:17 +0300

    some changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
 const path = require("path");
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/,
+module.exports = (env) =>{
+  const isProduction = env === "production"
+  return(
+    {
+      entry: "./src/app.js",
+      output: {
+        path: path.join(__dirname, "public"),
+        filename: "bundle.js",
       },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+      module: {
+        rules: [
+          {
+            loader: "babel-loader",
+            test: /\.js$/,
+            exclude: /node_modules/,
+          },
+          {
+            test: /\.scss$/,
+            use: ["style-loader", "css-loader", "sass-loader"],
+          },
+        ],
       },
-    ],
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-  },
-};
+      devtool:
+      isProduction ? "source-map":
+      "cheap-module-eval-source-map",
+      devServer: {
+        contentBase: path.join(__dirname, "public"),
+      },
+    }
+  )
+}
